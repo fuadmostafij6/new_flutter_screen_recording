5b91c032a07d8be6b82102ac56383e4273f4c31c
fuadmostfij6 <fuadmostfij4@gmail.com>
2023-04-30 20:00:51 +0600

    test

---
+++ lib/new_flutter_screen_recording.dart
@@ -0,0 +1,68 @@
+library new_flutter_screen_recording;
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
+import 'package:new_flutter_screen_recording/platform_interface/flutter_screen_recording_platform_interface.dart';
+
+
+class FlutterScreenRecording {
+  static Future<bool> startRecordScreen(String name, {required String titleNotification, required String messageNotification}) async{
+    await _maybeStartFGS(titleNotification, messageNotification);
+    final bool start = await FlutterScreenRecordingPlatform.instance.startRecordScreen(name);
+    return start;
+  }
+
+  static Future<bool> startRecordScreenAndAudio(String name, {required String titleNotification, required String messageNotification}) async {
+    //await _maybeStartFGS(titleNotification, messageNotification);
+    final bool start = await FlutterScreenRecordingPlatform.instance.startRecordScreenAndAudio(name);
+    return start;
+  }
+
+  static Future<String> get stopRecordScreen async {
+    final String path = await FlutterScreenRecordingPlatform.instance.stopRecordScreen;
+    if (!kIsWeb && Platform.isAndroid) {
+      FlutterForegroundTask.stopService();
+    }
+    return path;
+  }
+
+  static  _maybeStartFGS(String titleNotification, String messageNotification) async {
+    if (!kIsWeb && Platform.isAndroid) {
+
+       FlutterForegroundTask.init(
+        androidNotificationOptions: AndroidNotificationOptions(
+          channelId: 'notification_channel_id',
+          channelName: titleNotification,
+          channelDescription: messageNotification,
+          channelImportance: NotificationChannelImportance.LOW,
+          priority: NotificationPriority.LOW,
+          iconData: const NotificationIconData(
+            resType: ResourceType.mipmap,
+            resPrefix: ResourcePrefix.ic,
+            name: 'launcher',
+          ),
+          buttons: [
+            // const NotificationButton(id: 'sendButton', text: 'Send'),
+            // const NotificationButton(id: 'testButton', text: 'Test'),
+          ],
+        ),
+        iosNotificationOptions: const IOSNotificationOptions(
+          showNotification: true,
+          playSound: false,
+        ),
+        foregroundTaskOptions: const ForegroundTaskOptions(
+          interval: 5000,
+          autoRunOnBoot: true,
+          allowWifiLock: true,
+        ),
+
+      );
+    }
+  }
+
+  static void globalForegroundService() {
+    print("current datetime is ${DateTime.now()}");
+  }
+}
---
+++ test/new_flutter_screen_recording_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:new_flutter_screen_recording/new_flutter_screen_recording.dart';
+
+void main() {
+  test('adds one to input values', () {
+    // final calculator = Calculator();
+    // expect(calculator.addOne(2), 3);
+    // expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
+  });
+}
